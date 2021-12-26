98f36711f2d5712358b6b23669e35db9549791dd
DevomB <70069338+DevomB@users.noreply.github.com>
2021-12-26 15:26:20 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+var firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUnKi0inQcOi0_EbE3s6IgykzN1F0RSCQ",
+  authDomain: "open-source-f8e00.firebaseapp.com",
+  projectId: "open-source-f8e00",
+  storageBucket: "open-source-f8e00.appspot.com",
+  messagingSenderId: "503428000037",
+  appId: "1:503428000037:web:91b15332d15178e03f52cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
